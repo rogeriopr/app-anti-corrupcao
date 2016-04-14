a54deb145efaef1dda3c9ed1e10684a7ae7ca94d
davirdgs <davirdgs@MacBook-Pro-de-Davi.local>
2016-04-14 14:15:17 -0300

    Protoripo view Mais

---
+++ ios/Impactar/MaisTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MaisTableViewCell.swift
+//  Impactar
+//
+//  Created by Davi Rodrigues on 14/04/16.
+//  Copyright © 2016 Davi Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class MaisTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ios/Impactar/SliderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SliderTableViewCell.swift
+//  Impactar
+//
+//  Created by Davi Rodrigues on 14/04/16.
+//  Copyright © 2016 Davi Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class SliderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
